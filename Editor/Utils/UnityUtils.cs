ab83f09629c8d0d95b5318a3c28d5685585dee5b
ぷこるふ <86549420+puk06@users.noreply.github.com>
2025-06-26 14:41:37 +0900

    Add files via upload

---
+++ Editor/Utils/UnityUtils.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SmileyClip.Utils
+{
+    internal static class UnityUtils
+    {
+        /// <summary>
+        /// UnityのAssets内で、指定されたパスのファイルを開きます。
+        /// </summary>
+        /// <param name="assetPath"></param>
+        internal static void SelectAssetAtPath(string assetPath)
+        {
+            Object obj = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+            if (obj != null)
+            {
+                Selection.activeObject = obj;
+                EditorGUIUtility.PingObject(obj);
+            }
+            else
+            {
+                GuiUtils.ShowDialog($"指定されたパスのアセットが見つかりません:\n{assetPath}");
+            }
+        }
+    }
+}
