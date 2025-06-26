d651294c573e7c7a59c50411117333ae10214334
Puk06 <86549420+puk06@users.noreply.github.com>
2025-06-26 16:15:16 +0900

    build.jsの追加

---
+++ build.js
@@ -0,0 +1,36 @@
+const fs = require("node:fs");
+const archiver = require('archiver');
+const path = require("node:path");
+
+const PACKAGE_JSON = "package.json";
+const packageJson = JSON.parse(fs.readFileSync(PACKAGE_JSON, "utf8"));
+
+const PACKAGE_NAME = packageJson.name;
+const PACKAGE_VERSION = packageJson.version;
+
+const BUILD_DIR = './build';
+if (!fs.existsSync(BUILD_DIR)) {
+    fs.mkdirSync(BUILD_DIR);
+}
+
+const ZIP_FILE_NAME = path.join(BUILD_DIR, `${PACKAGE_NAME}-v${PACKAGE_VERSION}.zip`);
+if (fs.existsSync(ZIP_FILE_NAME)) {
+    fs.unlinkSync(ZIP_FILE_NAME);
+}
+
+const output = fs.createWriteStream(ZIP_FILE_NAME);
+const archive = archiver('zip', { zlib: { level: 9 } });
+
+archive.glob('**/*', {
+    ignore: ['build.js', '.gitignore', 'node_modules/**', '*.zip'],
+});
+
+output.on('close', () => {
+    console.log(`ZIP作成完了: ${archive.pointer()} total bytes`);
+});
+
+archive.on('error', err => { throw err; });
+
+archive.pipe(output);
+
+archive.finalize();
